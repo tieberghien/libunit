e1849c22b83913c080468cae0a862e0c4484accf
Wenceslas BARIDON <wbaridon@e3r2p3.42.fr>
2017-11-25 18:09:35 +0100

    Changement des tests de strcat

--- real-tests/strcat/02_space_test.c
+++ real-tests/strcat/02_space_test.c
@@ -1,24 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   01_basic_test.c                                    :+:      :+:    :+:   */
+/*   02_space_test.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wbaridon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/25 15:20:46 by wbaridon          #+#    #+#             */
-/*   Updated: 2017/11/25 18:07:32 by wbaridon         ###   ########.fr       */
+/*   Created: 2017/11/25 18:08:19 by wbaridon          #+#    #+#             */
+/*   Updated: 2017/11/25 18:08:27 by wbaridon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <string.h>
 
-int		basic_test(void)
+int		space_test(void)
 {
 	char	basic_dest[50];
 	char	basic_dest1[50];
-	char	basic_src = "Add this";
-	char	basic_src1 = "Add this";
+	char	basic_src = "   ";
+	char	basic_src1 = "   ";
 
 	if (ft_strcat(basic_dest, basic_src) == strcat(basic_dest1, basic_src1))
 		return (0);
--- real-tests/strcat/03_null_test.c
+++ real-tests/strcat/03_null_test.c
@@ -1,24 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   01_basic_test.c                                    :+:      :+:    :+:   */
+/*   03_null_test.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wbaridon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/25 15:20:46 by wbaridon          #+#    #+#             */
-/*   Updated: 2017/11/25 18:07:32 by wbaridon         ###   ########.fr       */
+/*   Created: 2017/11/25 18:08:35 by wbaridon          #+#    #+#             */
+/*   Updated: 2017/11/25 18:08:56 by wbaridon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <string.h>
 
-int		basic_test(void)
+int		null_test(void)
 {
 	char	basic_dest[50];
 	char	basic_dest1[50];
-	char	basic_src = "Add this";
-	char	basic_src1 = "Add this";
+	char	basic_src = "";
+	char	basic_src1 = "";
 
 	if (ft_strcat(basic_dest, basic_src) == strcat(basic_dest1, basic_src1))
 		return (0);
