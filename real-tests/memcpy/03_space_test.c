7f23e6ffe3a094dba99256116cd0f20f9450c07b
Wenceslas BARIDON <wbaridon@e3r2p3.42.fr>
2017-11-25 18:02:40 +0100

    Changement des tests memcpy

--- real-tests/memcpy/03_space_test.c
+++ real-tests/memcpy/03_space_test.c
@@ -1,24 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   01_basic_test.c                                    :+:      :+:    :+:   */
+/*   03_space_test.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wbaridon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/25 16:25:48 by wbaridon          #+#    #+#             */
-/*   Updated: 2017/11/25 18:00:21 by wbaridon         ###   ########.fr       */
+/*   Created: 2017/11/25 18:01:18 by wbaridon          #+#    #+#             */
+/*   Updated: 2017/11/25 18:01:46 by wbaridon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <string.h>
 
-int		basic_test(void)
+int		space_test(void)
 {
 	char	dest1[100];
 	char 	dest2[100];
 	
-	if (ft_memcpy(dest1, "Petit test", 5) == memcpy(dest2, "Petit test", 5))
+	if (ft_memcpy(dest1, "     ", 5) == memcpy(dest2, "     ", 5))
 		return (0);
 	else
 		return (-1)	
