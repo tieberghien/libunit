fe1ffefdd08a9b86e60cb128361b6bb13bf6f682
Wenceslas BARIDON <wbaridon@e3r2p3.42.fr>
2017-11-25 18:42:35 +0100

    Modif launcher atoi

--- real-tests/atoi/00_launcher.c
+++ real-tests/atoi/00_launcher.c
@@ -6,17 +6,31 @@
 /*   By: wbaridon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/25 11:07:32 by wbaridon          #+#    #+#             */
-/*   Updated: 2017/11/25 11:10:35 by wbaridon         ###   ########.fr       */
+/*   Updated: 2017/11/25 18:42:14 by wbaridon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // voir pour les includes
-
 #include "libunit.h"
 
 int		atoi_launcher(void)
 {
-	// a completer
+	int				i;
+	t_unit_test		*testlist;
+
+	i = 0;
 	ft_putstr("ATOI:");
+	load_test(&testlist, "Basic test", &basic_test, &i);
+	load_test(&testlist, "Null test", &null_test, &i);	
+	load_test(&testlist, "Negative number test", &negative_number_test, &i);
+	load_test(&testlist, "Negative int min test", &negative_int_min_test, &i);
+	load_test(&testlist, "Double negative test", &double_negative_test, &i);
+	load_test(&testlist, "Double positive test", &double_positive_test, &i);
+	load_test(&testlist, "Only space test", &only_space_test, &i);	
+	load_test(&testlist, "Space with number test", &space_with_number_test, &i);
+	load_test(&testlist, "Only letter  test", &only_letter_test, &i);
+	load_test(&testlist, "Number and letter test", &number_and_letter_test, &i);
+	load_test(&testlist, "One positive test", &one_positive_test, &i);
+	return (launch_tests(&testlist, i));
 	
 }
